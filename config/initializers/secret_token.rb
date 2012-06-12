ce9ea4a0d4889dd12986a4cf3c018174a15c36f1
Adam Breen <adam@adambreen.com>
2012-06-12 10:01:54 +1000

    initial poc - works on heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fitness::Application.config.secret_token = '8c6d4e8e2e88d753ea687489736f015c06a0f779401029d51d22cb3f374bcf5dbaeebcd713a723f94c469d317c8439a7c8ed0f641926319bf7dd4dde64b8ffb7'
